8176e7357217bff985f876e2dc8fdb8678bcd3ff
Nick Pitt <ngpitt@xphysics.net>
2017-12-18 11:58:25 -0500

    Updated client-go

--- pods/pods.go
+++ pods/pods.go
@@ -23,12 +23,12 @@ import (
 	"github.com/ngpitt/blinkt-k8s-controller/controller"
 	"github.com/ngpitt/blinkt-k8s-controller/helpers"
 
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
-	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/cache"
 )
 
